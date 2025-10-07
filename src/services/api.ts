01c026fabc714aa12af7ceeea229dd1c9591a3eb
Ritik-Raghav <ritikrghv313@gmail.com>
2025-10-07 20:28:30 +0530

    Enable credentials for API requests by setting withCredentials to true

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,6 +8,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json', // default; will override for FormData
   },
+  withCredentials: true,
 });
 
 // Request interceptor to add auth token if available
